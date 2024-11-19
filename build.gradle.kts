4129b0bcb96c051e048af26244adc71e9fad9411
rymarchik <insurg265@gmail.com>
2024-11-20 02:45:11 +0300

    Introduce DiscoveryService microservice

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.insurg"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.3")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
